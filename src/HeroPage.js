5392aa2996689c92498271f065658e1829e9816c
ngillca <sngill.ca@gmail.com>
2022-03-14 16:54:41 -0700

    Added the temporary image for the landing page

--- src/HeroPage.js
+++ src/HeroPage.js
@@ -1,14 +1,46 @@
 import React from "react";
 import { motion } from "framer-motion";
+import styles from "./Hero/HeroStyles.module.css";
+
+import logo from './The_Bots_GIF.gif'
 
 export default function HeroPage() {
   return (
-    <motion.div
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: 0.2 }}
-    >
-      <h2>Hello from the HeroPage component</h2>
-    </motion.div>
+    <>
+      <motion.div
+        className={styles.hero_container}
+        initial={{ x: "100vw" }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", delay: 0.2 }}
+      >
+        <h2>
+          Welcom to Bots Artifact - where you can explore and enjoy the latest
+          in STEM!
+        </h2>
+      </motion.div>
+
+      <motion.div
+        className={styles.hero_container}
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5 }}
+      >
+      
+        <img style={{ border: '1px solid white', borderRadius: '120px', width: '34%'}} src={logo} />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5 }}
+        className={styles.hero_landing1}
+      >
+        <div className={styles.item1}>
+          <h2>See the latest news in STEM, play games to enhance your problem
+          solving, and check out the blog to tickle your brain</h2>
+        </div>
+        <div className={styles.item2}><h2>Let's get started</h2></div>
+      </motion.div>
+    </>
   );
 }
